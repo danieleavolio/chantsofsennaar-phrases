787b4fc218174fdfe99a5f03c19f4b7171a94bd9
danieleavolio <daniele.avolio14@gmail.com>
2024-07-02 16:36:02 +0100

    'deploy'

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
@@ -194,7 +194,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "y8oahp"
+  version_hash: "1lyiyhf"
 };
 async function get_hooks() {
   return {};
