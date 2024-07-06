9a188250402b736754d9841bd5c1aa12a453c694
danieleavolio <daniele.avolio14@gmail.com>
2024-07-06 22:34:03 +0100

    'deploy'

---
+++ .vercel/output/static/_app/immutable/entry/start.CotcZJJl.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DItqlnxN.js";export{t as start};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import { dev } from "$app/environment";
     import { inject } from "@vercel/analytics";
 
-    inject();
+    inject({ mode: dev ? "development" : "production" });
 </script>
 
 <slot></slot>
